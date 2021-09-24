2f604e0e6fb4fa0f56386c096604815936408b85
satyssat <couyssat.satya@gmail.com>
2021-09-24 20:44:23 +0200

    Forum -> Affichage contenu question quasiment fini

--- actions/question/showArticleContentAction.php
+++ actions/question/showArticleContentAction.php
@@ -1,15 +1,27 @@
 <?php
 
-require('../database.php');
+require('actions/database.php');
 
+// Vérifier si l'id de la question dest rentrée dans l'url
 if (isset($_GET['id']) and !empty($_GET['id'])) {
-    $idOfQuestion = $_Get['id'];
+    //Récupérer l'id de la question
+    // $idOfQuestion = $_Get['id'];
 
-    $checkIfArticleExist = $bdd->prepare('SELECT * FROM question WHERE id = ?');
-    $checkIfArticleExist->execute($idOfQuestion);
+    // Récupérer infos question dont l'id est passé en paramètre
+    $checkIfAQuestionExist = $bdd->prepare('SELECT * FROM question WHERE id = ?');
+    $checkIfAQuestionExist->execute(array($_GET['id']));
 
-    if ($checkIfArticleExist->rowCount() > 0) {
-        $questionsInfo
+    // si un résultat à été trouvé 
+    if ($checkIfAQuestionExist->rowCount() > 0) {
+        //Stock toutes les infos dans £questionsINFO
+        $questionsInfos = $checkIfAQuestionExist->fetch();
+
+        // On attribue chaque données dans une variable
+        $questionTitle = $questionsInfos['titre'];
+        $questionContent = $questionsInfos['contenu'];
+        $questionIdAuthor = $questionsInfos['id_auteur'];
+        $questionPublicationDate = $questionsInfos['date_publication'];
+        $questionPseudoAuthor = $questionsInfos['pseudo_auteur'];
     } else {
         echo "aucune question n'a été trouvée";
     }
--- article.php
+++ article.php
@@ -1,4 +1,4 @@
-<?php include('actions/users/securityAction.php');
+<?php
 include('actions/question/showArticleContentAction.php'); ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -11,12 +11,13 @@ include("includes/head.php");
     include('includes/navbar.php');
     ?>
     <br></br>
-
     <div class="container">
-        <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Contenu de la question</label>
-            <textarea class="form-control" name="content"><?= $contenOfQuestion ?></textarea>
-        </div>
+        <?php
+        if (isset($questionPublicationDate)) {
+            $questionTitle;
+        }
+
+        ?>
     </div>
 </body>
 
